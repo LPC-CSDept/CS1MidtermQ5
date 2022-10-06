da03c37e16e2078e0b1060e8a1c1c6604deed046
Kyuwoong Lee <kwooler@gmail.com>
2022-10-06 15:59:01 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 using namespace std;
 
-
 int main()
 {
+	const int SIZE = 100;
+	int num1[SIZE], num2[SIZE];
 
-	/*********************************
-	Make Your Code
-	*********************************/
+	int N, M, i, j;
+
+	cout << "Enter the number of elements for the array Num1: ";
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cout << "Enter an integer value for the array Num1: ";
+		cin >> num1[i];
+	}
+
+	cout << "Enter the number of elements for the array Num2: ";
+	cin >> M;
+	for (int i = 0; i < M; i++)
+	{
+		cout << "Enter an integer value for the array Num2: ";
+		cin >> num2[i];
+	}
+	
+	/****************************************
+	Make Your Code Here
+	****************************************/
+	cout << "False" << endl; // Or
+	cout << "True" << endl;
 }
